ce99dd2329d59d72e8a4ca012e7639818593c824
Georgy Ginzburg <ginzburggeorgy@gmail.com>
2023-11-21 17:20:01 +0200

    macos: select theme based on system theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,20 @@
+local LIGHT_THEME = "github_light"
+local DARK_THEME = "catppuccin"
+
 function LoadTheme(theme_name)
 	vim.cmd.colorscheme(theme_name)
-	print("Loaded theme: "..theme_name)
+	print("Loaded theme: " .. theme_name)
 end
 
-LoadTheme("github_light")
+local OS_NAME = vim.fn.system { 'uname' }:gsub("%s+", "")
+
+if OS_NAME == "Darwin" then
+	if vim.fn.system { 'defaults', 'read', '-g', 'AppleInterfaceStyle' }:gsub("%s+", "") == "Dark" then
+		LoadTheme(DARK_THEME)
+	else
+		LoadTheme(LIGHT_THEME)
+	end
+	return
+end
+
+LoadTheme(LIGHT_THEME)
